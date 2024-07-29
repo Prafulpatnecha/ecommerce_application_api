6847cfe6ddee28e9565c8abde6450b6227b1ecc3
Praful Patnecha <prafulpatnecha@gmail.com>
2024-07-29 11:20:14 +0530

    29/7/24

---
+++ lib/controller/controlling.dart
@@ -0,0 +1,15 @@
+import 'package:ecommerce_application_api/controller/routes.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: AppRoutes.routes,
+    );
+  }
+}
